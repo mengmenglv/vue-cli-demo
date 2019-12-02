fc81c86a253eb3ff444cf6aefe89ff2b3b0ddce9
mengmenglv <lvmengmeng@163.com>
2019-12-02 10:34:00 +0800

    初次提交

---
+++ src/views/Info.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        this is info
+        <button type="button" @click="add()"></button>
+    </div>
+</template>
+
+<script>
+    import store from '@/store/index'
+
+    export default {
+        name: "Info",
+        store,
+        methods: {
+            add () {
+               console.log('add');
+               store.commit('increase')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
